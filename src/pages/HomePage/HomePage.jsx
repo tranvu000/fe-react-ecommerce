fcb1f191cce188d9665c4f08f6281e0cbe5cbb69
Vu Tran <tranvietvu0218@gmail.com>
2023-12-29 10:55:28 +0700

    Create image sliders automatically with react-slick

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import TypeProduct from "../../components/TypeProduct/TypeProduct";
 import { WrapperTypeProduct } from "./style";
+import SliderComponent from "../../components/SliderComponent/SliderComponent";
+import slider1 from "../../assets/images/slider1.jpg";
+import slider2 from "../../assets/images/slider2.jpg";
+import slider3 from "../../assets/images/slider3.jpg";
 
 const HomePage = () => {
   const arr = ['TV', 'Điện Thoại', 'Quần Áo', 'Thời Trang Nữ', 'Gia Dụng', 'Giày']
   return (
-    <div style={{ padding: '0 120px'}}>
-      <WrapperTypeProduct>
-        {arr.map((item) => {
-          return (
-            <TypeProduct name={item} key={item} />
-          )
-        })}
-      </WrapperTypeProduct>
-      HomePage
+    <>
+      <div style={{ padding: "0 120px"}}>
+        <WrapperTypeProduct>
+          {arr.map((item) => {
+            return (
+              <TypeProduct name={item} key={item} />
+            )
+          })}
+        </WrapperTypeProduct>
       </div>
+      <div id="container" style={{ backgroundColor: "#efefef", padding: "0 120px"}}>
+        <SliderComponent arrImages={[slider1, slider2, slider3]} />
+      </div>
+    </>
   )
 };
 
--- src/pages/HomePage/style.js
+++ src/pages/HomePage/style.js
@@ -5,4 +5,6 @@ export const WrapperTypeProduct = styled.div`
   align-items: center;
   gap: 24px;
   justify-content: flex-start;
+  border-bottom: 1px solid red;
+  height: 44px;
 `
