4266812ffa5c8f88ae43fe17481d1ab54b986307
Vu Tran <tranvietvu0218@gmail.com>
2023-12-29 22:59:34 +0700

    Create a Navbar section for the project

---
+++ src/components/NavBarComponent/style.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const WrapperLabelText = styled.h4`
+  color: rgb(56, 56, 61);
+  font-size: 14px;
+  font-weight: 500px;
+`
+
+export const WrapperTextValue = styled.span`
+  color: rgb(56, 56, 61);
+  font-size: 12px;
+  font-weight: 400px;
+`
+
+export const WrapperContent = styled.div`
+  display: flex;
+  // align-items: center;
+  flex-direction: column;
+  gap: 12px
+`
+
+export const WrapperTextPrice = styled.div`
+  padding: 4px;
+  borderRadius: 10px;
+  color: rgb(56, 56, 61),;
+  backgroundColor: rgb(238, 238, 238);
+  width: fit-content
+`
