c636fc4fb0e4c86c0e122825b1fb7fa5417aeb95
Vu Tran <tranvietvu0218@gmail.com>
2024-01-18 15:12:24 +0700

    add-redux-persist-order

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import productReducer from './slides/productSlide';
 import userReducer from './slides/userSlide';
 import orderReducer from './slides/orderSlide';
 
-export const store = configureStore({
-  reducer: {
-    product: productReducer,
-    user: userReducer,
-    order: orderReducer
-  },
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+  blacklist: ['product', 'user']
+};
+
+const rootReducer = combineReducers ({
+  product: productReducer,
+  user: userReducer,
+  order: orderReducer
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export let persistor = persistStore(store);
